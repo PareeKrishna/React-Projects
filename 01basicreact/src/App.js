2258f7d6e65379006a3ad997f3e378d0b359effc
KRISHNA <krishnapareek049@gmail.com>
2025-11-24 16:19:35 +0530

    created a basic react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Youtube from "./Youtube.jsx";
+
+function App() {
+  return (
+    <>
+      <h1>This is App Component</h1>
+      <h1>calssic react app</h1>
+      <Youtube />
+    </>
+  );
+}
+
+export default App;
