344e1867af83aa269b383b3c5aad44a4bfc7f06a
KRISHNA <krishnapareek049@gmail.com>
2025-11-26 14:56:45 +0530

    created a custom react app

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./App.jsx";
 
-import App from './App.jsx'
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google.com",
+};
 
-createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
-)
+const AnotherElement = (
+  <a href="https://google.com" target="_blank">
+    {" "}
+    Visit Google{" "}
+  </a>
+);
+
+function MyApp() {
+  return (
+    <div>
+      <h1> Custom React App</h1>
+    </div>
+  );
+}
+
+const arectElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click me to visit google.com"
+);
+createRoot(document.getElementById("root")).render(arectElement);
